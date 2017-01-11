4e7e5b83067c76c461d6ffd200081411051114dd
Rugile Pevceviciute <rugilena@gmail.com>
2017-01-11 22:15:11 +0000

    This should work now

--- elpis-pi-setup.sh
+++ elpis-pi-setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt install -y python-pip
 sudo apt-get install -y build-essentials
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y python-dev
-sudo apt-get install -y ansible
 sudo pip install ansible
 sudo pip install markupsafe
-
