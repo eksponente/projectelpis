63f0a40508d72316d56f78c86ed850f922294204
Rugile Pevceviciute <rugilena@gmail.com>
2017-01-14 21:00:51 +0000

    Fix pom

--- piSetup/elpis-pi-setup.sh
+++ piSetup/elpis-pi-setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt install -y python-pip
 sudo apt-get install -y build-essentials
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y python-dev
+sudo apt-get install -y default-jdk
 sudo pip install ansible
 sudo pip install markupsafe
 sudo ansible-playbook -i "localhost," -c local rpiSetup.yml
+
+sudo apt install -y oracle-java8-installer
+sudo apt install -y oracle-java8-set-default
+sudo apt-get install -y maven
